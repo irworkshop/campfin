44fba8a53b3fa30dbb0c7a7f60b70638d9160110
Kiernan Nicholls <kiernann@protonmail.com>
2019-07-16 14:41:33 -0400

    Create dataset documentation for geo and na_city

---
+++ R/data.R
@@ -0,0 +1,24 @@
+#' US City, State, and ZIPs
+#'
+#' A dataset containing all US ZIP codes alongside their city and state
+#'
+#' @format A tibble with 44,360 rows of 3 variables:
+#' \describe{
+#'   \item{city}{City name}
+#'   \item{state}{state abbreviation}
+#'   \item{zip}{US ZIP code}
+#'   ...
+#' }
+#' @details The original author claims this dataset contains over 98% of all US ZIP codes and covers 99.9% of the population. It may contains up to 0.5% false positives.
+#' @source Daniel Coven's federalgovernmentzipcodes.us web site and the CivicSpace US ZIP Code Database written by Schuyler Erle <schuyler@geocoder.us>, 5 August 2004. Original CSV files available from \url{http://federalgovernmentzipcodes.us/free-zipcode-database-Primary.csv}, \url{http://www.boutell.com/zipcodes/}, and \url{http://mappinghacks.com/data/}.
+"geo"
+
+#' Invalid City Values
+#'
+#' A vector containing common invalid city names
+#'
+#' @format A vector of length 54:
+#' @details The original author claims this dataset contains over 98% of all US ZIP codes and covers 99.9% of the population. It may contains up to 0.5% false positives.
+#' @source Daniel Coven's federalgovernmentzipcodes.us web site and the CivicSpace US ZIP Code Database written by Schuyler Erle <schuyler@geocoder.us>, 5 August 2004. Original CSV files available from \url{http://federalgovernmentzipcodes.us/free-zipcode-database-Primary.csv}, \url{http://www.boutell.com/zipcodes/}, and \url{http://mappinghacks.com/data/}.
+"na_city"
+
