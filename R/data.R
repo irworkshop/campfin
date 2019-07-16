a017cc044fa80908cca29168b547b45fa3b87de7
Kiernan Nicholls <kiernann@protonmail.com>
2019-07-16 16:39:00 -0400

    Create a data set of USPS C1 abbreviations

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' US City, State, and ZIPs
 #'
-#' A dataset containing all US ZIP codes alongside their city and state
+#' A dataset containing all US ZIP codes alongside their city and state.
 #'
 #' @format A tibble with 44,360 rows of 3 variables:
 #' \describe{
@@ -9,16 +9,33 @@
 #'   \item{zip}{US ZIP code}
 #'   ...
 #' }
-#' @details The original author claims this dataset contains over 98% of all US ZIP codes and covers 99.9% of the population. It may contains up to 0.5% false positives.
-#' @source Daniel Coven's federalgovernmentzipcodes.us web site and the CivicSpace US ZIP Code Database written by Schuyler Erle <schuyler@geocoder.us>, 5 August 2004. Original CSV files available from \url{http://federalgovernmentzipcodes.us/free-zipcode-database-Primary.csv}, \url{http://www.boutell.com/zipcodes/}, and \url{http://mappinghacks.com/data/}.
+#' @details The original author claims this dataset contains over 98% of all US ZIP codes and
+#'     covers 99.9% of the population. It may contains up to 0.5% false positives.
+#' @source Daniel Coven's federalgovernmentzipcodes.us web site and the CivicSpace US ZIP Code
+#'     Database written by Schuyler Erle <schuyler@geocoder.us>, 5 August 2004. Original CSV
+#'     files available from
+#'     \url{http://federalgovernmentzipcodes.us/free-zipcode-database-Primary.csv},
+#'     \url{http://www.boutell.com/zipcodes/}, and
+#'     \url{http://mappinghacks.com/data/}.
 "geo"
 
 #' Invalid City Values
 #'
-#' A vector containing common invalid city names
+#' A vector containing common invalid city names.
 #'
 #' @format A vector of length 54:
-#' @details The original author claims this dataset contains over 98% of all US ZIP codes and covers 99.9% of the population. It may contains up to 0.5% false positives.
-#' @source Daniel Coven's federalgovernmentzipcodes.us web site and the CivicSpace US ZIP Code Database written by Schuyler Erle <schuyler@geocoder.us>, 5 August 2004. Original CSV files available from \url{http://federalgovernmentzipcodes.us/free-zipcode-database-Primary.csv}, \url{http://www.boutell.com/zipcodes/}, and \url{http://mappinghacks.com/data/}.
 "na_city"
 
+#' US City, State, and ZIPs
+#'
+#' A dataset containing common street suffixes or suffix abbreviations and their full equivalent.
+#'
+#' @format A tibble with 44,360 rows of 3 variables:
+#' \describe{
+#'   \item{abb}{Commonly Used Street Suffix or Abbreviation}
+#'   \item{full}{Primary Street Suffix}
+#'   ...
+#' }
+#' @source The USPS C1 Street Suffix Abbreviations
+#'     \url{https://pe.usps.com/text/pub28/28apc_002.htm}
+"usps"
