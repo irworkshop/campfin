6a6b8e55d692250b90f4b8868410383a222bb324
Kiernan Nicholls <kiernann@protonmail.com>
2019-07-30 12:59:43 -0400

    Fix head to chose nbar

--- R/explore_bar.R
+++ R/explore_bar.R
@@ -9,13 +9,13 @@
 #' @importFrom ggplot2 ggplot geom_col scale_fill_brewer scale_y_continuous aes
 #' @importFrom dplyr count mutate desc
 #' @export
-explore_plot <- function(data, var, flip = FALSE, head = 8, palette = "Set1", ...) {
+explore_plot <- function(data, var, flip = FALSE, nbar = 8, palette = "Set1", ...) {
   var_string <- deparse(substitute(var))
   title_var <- snakecase::to_title_case(var_string)
   base_plot <- data %>%
     dplyr::count({{ var }}, sort = TRUE) %>%
     dplyr::mutate(p = n/sum(n)) %>%
-    base::head(head) %>%
+    utils::head(nbar) %>%
     ggplot2::ggplot(ggplot2::aes(reorder({{ var }}, dplyr::desc(p)), p)) +
     ggplot2::geom_col(ggplot2::aes(fill = {{ var }})) +
     ggplot2::scale_fill_brewer(palette = palette, guide = FALSE) +
