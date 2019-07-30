2c1fd6214d22e97553205014994e7cd97a4cd376
Kiernan Nicholls <kiernann@protonmail.com>
2019-07-30 12:37:00 -0400

    Impliment head and custom pallete

--- R/explore_bar.R
+++ R/explore_bar.R
@@ -9,15 +9,16 @@
 #' @importFrom ggplot2 ggplot geom_col scale_fill_brewer scale_y_continuous aes
 #' @importFrom dplyr count mutate desc
 #' @export
-explore_plot <- function(data, var, flip = FALSE, ...) {
+explore_plot <- function(data, var, flip = FALSE, head = 8, palette = "Set1", ...) {
   var_string <- deparse(substitute(var))
   title_var <- snakecase::to_title_case(var_string)
   base_plot <- data %>%
     dplyr::count({{ var }}, sort = TRUE) %>%
     dplyr::mutate(p = n/sum(n)) %>%
+    base::head(head) %>%
     ggplot2::ggplot(ggplot2::aes(reorder({{ var }}, dplyr::desc(p)), p)) +
     ggplot2::geom_col(ggplot2::aes(fill = {{ var }})) +
-    ggplot2::scale_fill_brewer(palette = "Set1", guide = FALSE) +
+    ggplot2::scale_fill_brewer(palette = palette, guide = FALSE) +
     ggplot2::scale_y_continuous(labels = scales::percent) +
     ggplot2::labs(
       x = title_var,
