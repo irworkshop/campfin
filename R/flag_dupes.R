8dcfea9c02ff8cdc3b54fe6aac3fee395aa60ea5
Kiernan Nicholls <kiernann@protonmail.com>
2019-08-05 11:40:34 -0400

    Add example for flag_dupe()

--- R/flag_dupes.R
+++ R/flag_dupes.R
@@ -3,6 +3,8 @@
 #' @param df A data frame
 #' @return A data frame with a new `dupe_flag` logical variable
 #' @importFrom dplyr mutate
+#' @example
+#' sum(flag_dupes(iris)$dupe_flag)
 #' @export
 flag_dupes <- function(df) {
   flagged_data <- dplyr::mutate(df, dupe_flag = base::duplicated(df))
