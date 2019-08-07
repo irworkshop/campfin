6bdab1080813ae23f535f6a398037f5b4736b724
Kiernan Nicholls <kiernann@protonmail.com>
2019-08-07 17:26:17 -0400

    Create function to flag NA in select variables

---
+++ R/flag_na.R
@@ -0,0 +1,11 @@
+#' Flag Missing Values
+#'
+#' @param df A data frame
+#' @return A data frame with a new `na_flag` logical variable
+#' @importFrom dplyr mutate select
+#' @example
+#' flag_na(dplyr::storms, ts_diameter)
+#' @export
+flag_na <- function(data, ...) {
+  dplyr::mutate(data, na_flag = !complete.cases(dplyr::select(data, ...)))
+}
