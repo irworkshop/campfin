9f753afb52ab5728c05c4b987b5923533b2b276f
Kiernan Nicholls <kiernann@protonmail.com>
2019-08-08 17:02:31 -0400

    Create function to abbreviate state names

---
+++ R/abrev_state.R
@@ -0,0 +1,14 @@
+#' AbbreviateUS State Names
+#'
+#' @param state A vector of full state names
+#' @return A vector of 2 letter state abbreviations
+#' @importFrom stringr str_to_upper
+#' @example
+#' abrev_state(c("Vermont", "District of Columbia"))
+#' @export
+abrev_state <- function(state) {
+  state <- stringr::str_to_upper(state)
+  abb   <- stringr::str_to_upper(c(state.abb, "DC"))
+  name  <- stringr::str_to_upper(c(state.name, "District of Columbia"))
+  abb[match(state, name)]
+}
