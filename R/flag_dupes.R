b6862879aaf754bbda7e0221525b1b96feb397fc
Kiernan Nicholls <kiernann@protonmail.com>
2019-08-08 11:53:05 -0400

    Allow for passing of arguments to select()

--- R/flag_dupes.R
+++ R/flag_dupes.R
@@ -1,12 +1,13 @@
 #' Flag Duplicate Rows
 #'
 #' @param df A data frame
+#' @param  ... Arguments to pass to dplyr::select() (e.g., dplyr::everything())
 #' @return A data frame with a new `dupe_flag` logical variable
 #' @importFrom dplyr mutate
 #' @example
 #' sum(flag_dupes(iris)$dupe_flag)
 #' @export
-flag_dupes <- function(df) {
-  flagged_data <- dplyr::mutate(df, dupe_flag = base::duplicated(df))
+flag_dupes <- function(df, ...) {
+  flagged_data <- dplyr::mutate(df, dupe_flag = base::duplicated(dplyr::select(df, ...)))
   return(flagged_data)
 }
