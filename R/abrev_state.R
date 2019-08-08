94d5c8e74670aea7323e41f28657a15ee0bf6f7c
Kiernan Nicholls <kiernann@protonmail.com>
2019-08-08 17:11:34 -0400

    Perform more state normalization before match

--- R/abrev_state.R
+++ R/abrev_state.R
@@ -8,6 +8,10 @@
 #' @export
 abrev_state <- function(state) {
   state <- stringr::str_to_upper(state)
+  state <- stringr::str_remove_all(state, "[:punct:]")
+  state <- stringr::str_remove_all(state, "\\d+")
+  state <- stringr::trim(state)
+  state <- stringr::squish(state)
   abb   <- stringr::str_to_upper(c(state.abb, "DC"))
   name  <- stringr::str_to_upper(c(state.name, "District of Columbia"))
   abb[match(state, name)]
