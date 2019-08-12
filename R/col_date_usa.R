4335e6eb108c746bbb625ee933052bd9ec2c8bef
Kiernan Nicholls <kiernann@protonmail.com>
2019-08-12 11:39:56 -0400

    Create function for USA MDY default col date

---
+++ R/col_date_usa.R
@@ -0,0 +1,11 @@
+#' Parse USA (MDY) Dates
+#'
+#' @description Parse MM/DD/YYY dates
+#' @return A `POSIXct()` vector
+#' @importFrom readr col_date
+#' @examples
+#' readr::read_csv(file = "x\n09/18/1996", col_types = cols(x = col_date_usa()))
+#' @export
+col_date_usa <- function() {
+  col_date(format = "%m/%d/%Y")
+}
