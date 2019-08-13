651d5eef5db2fc1d1602cd72c40642a88e7e4fac
Kiernan Nicholls <kiernann@protonmail.com>
2019-08-13 14:21:01 -0400

    Import cols from readr for example

--- R/col_date_usa.R
+++ R/col_date_usa.R
@@ -2,9 +2,9 @@
 #'
 #' @description Parse MM/DD/YYY dates
 #' @return A `POSIXct()` vector
-#' @importFrom readr col_date
+#' @importFrom readr col_date cols
 #' @examples
-#' readr::read_csv(file = "x\n09/18/1996", col_types = cols(x = col_date_usa()))
+#' readr::read_csv(file = "x\n09/18/1996", col_types = readr::cols(x = col_date_usa()))
 #' @export
 col_date_usa <- function() {
   readr::col_date(format = "%m/%d/%Y")
