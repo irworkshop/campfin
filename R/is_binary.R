947d81980580aeeabea65b784829df48f74ac55f
Kiernan Nicholls <kiernann@protonmail.com>
2019-08-13 15:15:16 -0400

    Remove NA from is_binary by default

--- R/is_binary.R
+++ R/is_binary.R
@@ -1,11 +1,13 @@
 #' Is Binary
 #'
 #' @param x A vector
+#' @param na.rm Should NA be ignored, `TRUE` by default
 #' @return TRUE if only 2 unique values
 #' @importFrom dplyr n_distinct
 #' @examples
 #' is_binary(x = c("Yes", "No"))
 #' @export
-is_binary <- function(x) {
+is_binary <- function(x, na.rm = TRUE) {
+  if (na.rm) x <- na.omit(x)
   dplyr::n_distinct(x) == 2
 }
