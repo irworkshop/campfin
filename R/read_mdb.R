7bc349dcbd0ffbe92bbbed71d719d768981dfc80
Kiernan Nicholls <kiernann@protonmail.com>
2019-08-15 13:57:40 -0400

    Add mdb_tables and improve documentation

--- R/read_mdb.R
+++ R/read_mdb.R
@@ -1,12 +1,25 @@
-#' Ready Microsoft Access Tables
+#' Read Microsoft Access Table
 #'
-#' @description Wraps around `readr::read_csv()` and MDB Tools
+#' @description Wraps around `readr::read_csv()` and `mdb-export`
+#' @param file A path to a .mdb file
+#' @param table A character string to a table name (see `mdb_tables()`)
+#' @param ... Arguments to be passed to `readr::read_csv()`
 #' @return A tibble
 #' @importFrom readr read_csv
 #' @export
-read_mdb <- function(data, table, ...) {
+read_mdb <- function(file, table, ...) {
   read_csv(
-    file = system(command = paste("mdb-export", data, table), intern = TRUE),
+    file = system(command = paste("mdb-export", file, table), intern = TRUE),
     ...
   )
+}
+
+#' Get Tables from Microsoft Access File
+#'
+#' @description Wraps around `mdb-tables`
+#' @param file A path to a .mdb file
+#' @return A vector of table names
+#' @importFrom readr read_csv
+mdb_tables <- function(file) {
+  system(paste("mdb-tables -1", file), intern = TRUE)
 }
