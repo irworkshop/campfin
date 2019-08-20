27b2d5a123286c868ff47a5cc409eb0bccfa6e8e
Kiernan Nicholls <kiernann@protonmail.com>
2019-08-20 13:10:05 -0400

    Create single file version of all_files_new()

--- R/all_files_new.R
+++ R/all_files_new.R
@@ -1,4 +1,4 @@
-#' Check for All New Files
+#' Check if All Files Are New
 #'
 #' @param path The path to a directory to check.
 #' @param glob A pattern to search for files (e.g., "csv").
@@ -22,4 +22,22 @@ all_files_new <- function(path, glob = NULL, ...) {
     base::warning("Empty")
     base::return(FALSE)
   }
+}
+
+#' Check if a File is New
+#'
+#' @param path The path to a file to check.
+#' @return A logical value indicating whether or a file has modification date equal to today.
+#' @importFrom lubridate today
+#' @importFrom magrittr use_series equals
+#' @export
+this_file_new <- function(path) {
+  if (!file.exists(path)) {
+    warning("File does not exist")
+    new <- FALSE
+  } else {
+    mod_day <- lubridate::floor_date(file.mtime(path), unit = "day")
+    new <- mod_day == lubridate::today()
+  }
+  return(new)
 }
