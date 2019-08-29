4f82ce79b4642ab5c0fb1d7dc9f39cd37d3a7153
Kiernan Nicholls <kiernann@protonmail.com>
2019-08-29 12:44:31 -0400

    Improve documentation of %out%

--- R/out.R
+++ R/out.R
@@ -1,10 +1,7 @@
 #' Inverted Match
 #'
-#' @param x A vector to check.
-#' @param y A vector to compare against.
 #' @return Logical if X is not present in Y
 #' @examples
-#' c("A", "B", "0") %out% LETTERS
-#' @usage x %out% y
+#' c("A", "B", "3") %out% LETTERS
 #' @export
 "%out%" <- Negate("%in%")
