0ff9f26b0660e1f6e60564b0e813663b7b2ecbfc
Kiernan Nicholls <kiernann@protonmail.com>
2019-08-29 16:30:26 -0400

    Improve col_date_usa documentation

--- R/col_date_usa.R
+++ R/col_date_usa.R
@@ -1,7 +1,9 @@
-#' Parse USA (MDY) Dates
+#' Parse USA Date Columns in readr Functions
 #'
-#' @description Parse MM/DD/YYY dates
-#' @return A `POSIXct()` vector
+#' @description Parse dates with format MM/DD/YYYY. This function simply wraps around
+#' [readr::col_date()] with the `format` argument set to `"%m/%d/%Y"`. Most US campaign finance
+#' datasets use this format. Does not work with [vroom::vroom()] version 1.0.2.9000 or lower.
+#' @return A `POSIXct()` vector.
 #' @importFrom readr col_date cols
 #' @examples
 #' readr::read_csv(file = "x\n09/18/1996", col_types = readr::cols(x = col_date_usa()))
