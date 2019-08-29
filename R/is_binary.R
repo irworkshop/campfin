377dea9cf88c0dd638fb5e8dd2ca8c783091d746
Kiernan Nicholls <kiernann@protonmail.com>
2019-08-29 16:50:28 -0400

    Improve is_binary documentation and example

--- R/is_binary.R
+++ R/is_binary.R
@@ -1,11 +1,13 @@
 #' Is Binary
 #'
-#' @param x A vector
-#' @param na.rm Should NA be ignored, `TRUE` by default
-#' @return TRUE if only 2 unique values
+#' @description uses [dplyr::n_distinct()] to check if there are only two unique values. This
+#' function is useful when combined with both [purrr::map_if()] _and_ [batman::to_logical()].
+#' @param x A vector.
+#' @param na.rm Should NA be ignored, `TRUE` by default.
+#' @return TRUE if only 2 unique values.
 #' @importFrom dplyr n_distinct
 #' @examples
-#' is_binary(x = c("Yes", "No"))
+#' if (is_binary(x <- c("Yes", "No"))) batman::to_logical(x)
 #' @export
 is_binary <- function(x, na.rm = TRUE) {
   if (na.rm) x <- na.omit(x)
