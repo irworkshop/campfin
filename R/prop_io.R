23d384b8bd9f6684a5b485baf4e9037173dcbad0
Kiernan Nicholls <kiernann@protonmail.com>
2019-08-29 13:26:17 -0400

    Import stats and simplify prop_io

--- R/prop_io.R
+++ R/prop_io.R
@@ -2,34 +2,27 @@
 #'
 #' @param x A vector to check.
 #' @param y A vector to compare against.
-#' @param na.rm Should NA values be ignored.
+#' @param na.rm Should `NA` be ignored?
 #' @return The proprtion of X present in Y.
+#' @importFrom stats na.omit
 #' @examples
 #' prop_in(c("VT", "NH", "ZZ", "ME"), state.abb)
 #' @export
 prop_in <- function(x, y, na.rm = TRUE) {
-  if (na.rm) {
-    prop <- mean(na.omit(x) %in% y)
-  } else {
-    prop <- mean(x %in% y)
-  }
-  return(prop)
+  if(na.rm) x <- stats::na.omit(x)
+  mean(x %in% y, na.rm = TRUE)
 }
 
 #' Proportion Out
 #'
 #' @param x A vector to check.
 #' @param y A vector to compare against.
-#' @param na.rm Should NA values be ignored.
+#' @param na.rm Should `NA` be ignored?
 #' @return The proprtion of X _not_ present in Y.
 #' @examples
 #' prop_in(c("VT", "NH", "ZZ", "ME"), state.abb)
 #' @export
 prop_out <- function(x, y, na.rm = TRUE) {
-  if (na.rm) {
-    prop <- mean(!(na.omit(x) %in% y))
-  } else {
-    prop <- mean(!(x %in% y))
-  }
-  return(prop)
+  if(na.rm) x <- stats::na.omit(x)
+  mean(x %out% y, na.rm = TRUE)
 }
