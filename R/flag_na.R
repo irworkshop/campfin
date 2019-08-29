bb7039f038952af86607b00a2f3f874fe383ec7e
Kiernan Nicholls <kiernann@protonmail.com>
2019-08-29 13:34:27 -0400

    Call stats::complete.cases explicitly

--- R/flag_na.R
+++ R/flag_na.R
@@ -4,9 +4,10 @@
 #' @param ... Column names passed to \code{dplyr::select()} and checked for \code{NA}
 #' @return A data frame with a new `na_flag` logical variable
 #' @importFrom dplyr mutate select
+#' @importFrom stats complete.cases
 #' @examples
 #' flag_na(dplyr::storms, ts_diameter)
 #' @export
 flag_na <- function(data, ...) {
-  dplyr::mutate(data, na_flag = !complete.cases(dplyr::select(data, ...)))
+  dplyr::mutate(data, na_flag = !stats::complete.cases(dplyr::select(data, ...)))
 }
