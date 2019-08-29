4d19be7fc0b8fa159075db26bec54fbc617f8060
Kiernan Nicholls <kiernann@protonmail.com>
2019-08-29 16:41:25 -0400

    Improve na_flag documentation

--- R/flag_na.R
+++ R/flag_na.R
@@ -1,12 +1,14 @@
-#' Flag Missing Values
+#' Flag Missing Values With New Column
 #'
-#' @param data A data frame
-#' @param ... Column names passed to \code{dplyr::select()} and checked for \code{NA}
-#' @return A data frame with a new `na_flag` logical variable
+#' @description This function uses [dplyr::mutate()] to create a new `dupe_flag` logical variable
+#' with `TRUE` values for any record duplicated more than once.
+#' @param data A data frame to flag.
+#' @param  ... Arguments passed to [dplyr::select()] (needs to be at least [dplyr::everything()]).
+#' @return A data frame with a new `na_flag` logical variable.
 #' @importFrom dplyr mutate select
 #' @importFrom stats complete.cases
 #' @examples
-#' flag_na(dplyr::storms, ts_diameter)
+#' flag_na(dplyr::starwars, gender)
 #' @export
 flag_na <- function(data, ...) {
   dplyr::mutate(data, na_flag = !stats::complete.cases(dplyr::select(data, ...)))
