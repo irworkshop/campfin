565c94b5db1800309e0694fda9dc7715f8662cc8
Kiernan Nicholls <kiernann@protonmail.com>
2019-08-29 12:31:31 -0400

    Create campfin package documentation

---
+++ R/campfin.R
@@ -0,0 +1,12 @@
+#' \code{campfin} package
+#'
+#' Wrangling Campaign Finance
+#'
+#' See the README on \href{https://github.com/kiernann/campfin/#readme}{GitHub}
+#'
+#' @docType package
+#' @name campfin
+#' @importFrom dplyr %>%
+NULL
+
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
