d6815a37672f507d6fe0e4a8a73fd2e14c692de4
Kiernan Nicholls <kiernann@protonmail.com>
2019-08-29 13:35:17 -0400

    Fix details code in %out%

--- R/out.R
+++ R/out.R
@@ -1,11 +1,10 @@
 #' Inverted Match
-#' @description \code{%out%} is an inverted version of the infix opperator \code{%in%}.
+#' @description `%out%` is an inverted version of the infix opperator `%in%`.
 #' @param x vector or NULL: the values to be matched. Long vectors are supported.
 #' @param table vector or NULL: the values to be matched against.
 #' @return Logical if X is not present in Y
 #' @examples
 #' c("A", "B", "3") %out% LETTERS
-#' @details \code{%out%} is currently defined at
-#' \code{"%out%" <- function(x, table) match(x, table, nomatch = 0) == 0}
+#' @details `%out%` is currently defined as `"%out%" <- function(x, table) match(x, table, nomatch = 0) == 0`
 #' @export
 "%out%" <- function(x, table) match(x, table, nomatch = 0) == 0
