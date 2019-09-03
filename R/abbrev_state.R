070cdad9c74546c60ea0be81150e3befd7939c15
Kiernan Nicholls <kiernann@protonmail.com>
2019-09-03 16:43:29 -0400

    Update abbre_state and expand_state

--- R/abbrev_state.R
+++ R/abbrev_state.R
@@ -1,44 +1,40 @@
-#' Abbreviate US State Names
-#'
-#' @description This function is used to first normalize a `full` state name and then return any
-#' matching abbreviation. It relies on [datasets::state.name] and [datasets::state.abb]
-#' (supplement with "District of Columbia" and "DC" respectively). Other US territories besides
-#' these 51 are not yet included, although their abbreviations are in the [geo] and [valid_state]
-#' objects.
+#' @title Abbreviate US State Names
+#' @description This function is used to first normalize a `full` state name and
+#'   then return any matching abbreviation. This package works for all state
+#'   names in `state.name` plus 12 additional territories (e.g., "Puerto Rico").
 #' @aliases abbreviate_state
 #' @param full A full US state name character vector (e.g., "Vermont").
-#' @param na_bad logical; Whether to return `NA` for invalid `full` names (default `FALSE`).
-#' @param rm_nums Whether to remove numbers from `full` (e.g., 1st) before abbreviating.
-#' Particularly useful with US Congressional districts. Only numbers 1 through 20 are supported at
-#' this time (default `FALSE`).
+#' @param na_bad logical; Whether to return `NA` for invalid `full` names
+#'   (default `FALSE`).
+#' @param rm_nums logical; Whether to remove numbers from `full` (e.g., 1st)
+#'   before abbreviating. Particularly useful with US Congressional districts.
+#'   Only numbers 1 through 20 are supported at this time (default `FALSE`).
 #' @return A vector of 2 letter state abbreviations.
 #' @importFrom stringr str_to_upper str_trim str_squish str_replace_all str_remove_all
 #' @examples
 #' abbrev_state(full = state.name)
-#' abbrev_state(full = c("Vermont", "District of Columbia", "France"), na_bad = TRUE)
+#' abbrev_state(full = c("new_mexico", "france"), na_bad = TRUE)
 #' @family geographic normalization functions
 #' @seealso abbrev_state is_abbrev normal_state normal_zip normal_city
 #' @export
 abbrev_state <- function(full, na_bad = FALSE, rm_nums = FALSE) {
-  valid_abb   <- c(datasets::state.abb, "DC")
-  valid_name  <- stringr::str_to_upper(c(datasets::state.name, "District of Columbia"))
-
+  if (!is.character(full)) {
+    stop("Full state name must be a character vector")
+  }
   full <- stringr::str_to_upper(full)
-  full <- stringr::str_trim(full)
-  full <- stringr::str_squish(full)
-
+  full <- stringr::str_replace(full, "[:punct:]", " ")
   if (rm_nums) {
     for (pattern in c("1ST", "2ND", "3RD", stringr::str_c(1:19, "TH"))) {
       full <- stringr::str_remove(full, pattern)
     }
     full <- stringr::str_remove_all(full, "\\d+")
   }
-
+  full <- stringr::str_trim(full)
+  full <- stringr::str_squish(full)
   if (na_bad) {
-    full[which(full %out% valid_name & full %out% valid_abb)] <- NA
+    full[which(full %out% valid_name & full %out% valid_state)] <- NA
   }
-
   abb_match <- match(full[which(full %in% valid_name)], valid_name)
-  full[which(full %in% valid_name)] <- valid_abb[abb_match]
+  full[which(full %in% valid_name)] <- valid_state[abb_match]
   return(full)
 }
--- R/expand_state.R
+++ R/expand_state.R
@@ -1,44 +1,40 @@
-#' Expand US State Abbreviations
-#'
-#' @description This function is used to first normalize a `abb` state abbreviation and then return
-#' any matching name It relies on [datasets::state.abb] and [datasets::state.name]
-#' (supplement with "DC" and "District of Columbia" respectively). Other US territories besides
-#' these 51 are not yet included, although their abbreviations are in the [geo] and [valid_state]
-#' objects.
-#' @aliases expand_abb expand_abbreviation
-#' @param abb A 2 digit US state abbreviation character vector (e.g., "VT").
-#' @param na_bad logical; Whether to return `NA` for invalid `abb` (default `FALSE`).
-#' @param rm_nums Whether to remove numbers from `abb` (e.g., 1st) before abbreviating.
-#' Particularly useful with US Congressional districts. Only numbers 1 through 20 are supported at
-#' this time (default `FALSE`).
-#' @return A vector of full state names.
+#' @title Abbreviate US State Names
+#' @description This function is used to first normalize a `abb` state name and
+#'   then return any matching abbreviation. This package works for all state
+#'   names in `state.name` plus 12 additional territories (e.g., "Puerto Rico").
+#' @param abb A full US state name character vector (e.g., "Vermont").
+#' @param na_bad logical; Whether to return `NA` for invalid `abb` names
+#'   (default `FALSE`).
+#' @param rm_nums logical; Whether to remove numbers from `abb` (e.g., 1st)
+#'   before abbreviating. Particularly useful with US Congressional districts.
+#'   Only numbers 1 through 20 are supported at this time (default `FALSE`).
+#' @return A vector of 2 letter state abbreviations.
 #' @importFrom stringr str_to_upper str_trim str_squish str_replace_all str_remove_all
 #' @examples
 #' expand_state(abb = state.abb)
-#' expand_state(abb = c("VT", "DC", "FR"), na_bad = TRUE)
+#' expand_state(abb = c("NM", "FR"), na_bad = TRUE)
+#' expand_state(abb = "NE03", rm_nums = TRUE)
 #' @family geographic normalization functions
 #' @seealso abbrev_state is_abbrev normal_state normal_zip normal_city
 #' @export
 expand_state <- function(abb, na_bad = FALSE, rm_nums = FALSE) {
-  valid_abb   <- c(datasets::state.abb, "DC")
-  valid_name  <- stringr::str_to_upper(c(datasets::state.name, "District of Columbia"))
-
+  if (!is.character(abb)) {
+    stop("Full state name must be a character vector")
+  }
   abb <- stringr::str_to_upper(abb)
-  abb <- stringr::str_trim(abb)
-  abb <- stringr::str_squish(abb)
-
+  abb <- stringr::str_replace(abb, "[:punct:]", " ")
   if (rm_nums) {
     for (pattern in c("1ST", "2ND", "3RD", stringr::str_c(1:19, "TH"))) {
       abb <- stringr::str_remove(abb, pattern)
     }
     abb <- stringr::str_remove_all(abb, "\\d+")
   }
-
+  abb <- stringr::str_trim(abb)
+  abb <- stringr::str_squish(abb)
   if (na_bad) {
-    abb[which(abb %out% valid_abb & abb %out% valid_name)] <- NA
+    abb[which(abb %out% valid_state & abb %out% valid_name)] <- NA
   }
-
-  full_match <- match(abb[which(abb %in% valid_abb)], valid_abb)
-  abb[which(abb %in% valid_abb)] <- valid_name[full_match]
+  abb_match <- match(abb[which(abb %in% valid_state)], valid_state)
+  abb[which(abb %in% valid_state)] <- valid_name[abb_match]
   return(abb)
 }
