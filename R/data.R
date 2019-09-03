1b4b8fa975bd87eb2c845c6b80d5448fe1de4b93
Kiernan Nicholls <kiernann@protonmail.com>
2019-09-03 16:36:51 -0400

    Create and document new data objects

--- R/data.R
+++ R/data.R
@@ -1,90 +1,99 @@
-#' US City, State, and ZIPs
-#'
-#' A dataset containing all US ZIP codes alongside their city and state.
-#'
-#' @format A tibble with 44,360 rows of 3 variables:
+#' @title US City, state, and ZIP
+#' @description This tibble is the third version of a popular zipcodes database.
+#'   The original CivicSpace US ZIP Code Database was created by Schuyler Erle
+#'   using ZIP code gazetteers from the US Census Bureau from 1999 and 2000,
+#'   augmented with additional ZIP code information from the Census Bureau’s
+#'   TIGER/Line 2003 data set. The second version was published as the
+#'   `zipcode::zipcode` dataframe object. This version has dropped the latitude
+#'   and longitude, reorganized columns, and normalize the city values with
+#'   [normal_city()].
+#' @format A tibble with 44,336 rows of 3 variables:
 #' \describe{
-#'   \item{city}{City name}
-#'   \item{state}{state abbreviation}
-#'   \item{zip}{US ZIP code}
+#'   \item{city}{Normalized city name.}
+#'   \item{state}{Two letter state abbreviation.}
+#'   \item{zip}{Five-digit ZIP Code.}
 #'   ...
 #' }
-#' @details The original author claims this dataset contains over 98% of all US ZIP codes and
-#'     covers 99.9% of the population. It may contains up to 0.5% false positives.
-#' @source Daniel Coven's federalgovernmentzipcodes.us web site and the CivicSpace US ZIP Code
-#'     Database written by Schuyler Erle <schuyler@geocoder.us>, 5 August 2004. Original CSV
-#'     files available from
-#'     \url{http://federalgovernmentzipcodes.us/free-zipcode-database-Primary.csv},
-#'     \url{http://www.boutell.com/zipcodes/}, and
-#'     \url{http://mappinghacks.com/data/}.
-"geo"
+#' @source Daniel Coven's federalgovernmentzipcodes.us web site and the
+#'   CivicSpace US ZIP Code Database written by Schuyler Erle
+#'   <schuyler@@geocoder.us>, 5 August 2004. Original CSV files available from
+#'   \url{http://federalgovernmentzipcodes.us/free-zipcode-database-Primary.csv},
+#'   \url{http://www.boutell.com/zipcodes/}, and
+#'   \url{http://mappinghacks.com/data/}.
+"zipcodes"
 
-#' Invalid City Values
-#'
-#' A vector containing common invalid city names.
-#'
-#' @format A vector of length 54:
-"na_city"
-
-#' USPS C1 Address Abbreviations
-#'
-#' A dataset containing common street suffixes or suffix abbreviations and their full equivalent.
-#'
-#' @format A tibble with 44,360 rows of 3 variables:
+#' @title USPS Street Abbreviations
+#' @description A tibble containing common street suffixes or suffix
+#'   abbreviations and their full equivalent. Useful as the `add_abbs` argument
+#'   of [normal_address()].
+#' @format A tibble with 325 rows of 3 variables:
 #' \describe{
-#'   \item{abb}{Commonly Used Street Suffix or Abbreviation}
+#'   \item{full}{Primary Street Suffix.}
+#'   \item{abb}{Commonly Used Street Suffix or Abbreviation.}
+#'   ...
+#' }
+#' @source USPS Appendix
+#'    [C1 Street Abbreviations](https://pe.usps.com/text/pub28/28apc_002.htm).
+"usps_street"
+
+#' @title USPS City Abbreviations
+#' @description A currated and edited subset of [usps_street] containing the
+#'   USPS abbreviations found in city names. Useful as the `geo_abbs` argument
+#'   of [normal_city()].
+#' @format A tibble with 154 rows of 2 variables:
+#' \describe{
 #'   \item{full}{Primary Street Suffix}
-#'   ...
-#' }
-#' @source The USPS C1 Street Suffix Abbreviations
-#'     \url{https://pe.usps.com/text/pub28/28apc_002.htm}
-"usps"
-
-#' USPS C1 City Abbreviations
-#'
-#' A currated and edited dataset containing the USPS abbreviations for only city names.
-#'
-#' @format A tibble with 44,360 rows of 3 variables:
-#' \describe{
 #'   \item{abb}{Commonly Used Street Suffix or Abbreviation}
-#'   \item{full}{Primary Street Suffix}
 #'   ...
 #' }
-#' @source The USPS C1 Street Suffix Abbreviations
-#'     \url{https://pe.usps.com/text/pub28/28apc_002.htm}
+#' @source USPS Appendix C1,
+#'    [Street Abbreviations](https://pe.usps.com/text/pub28/28apc_002.htm)
 "usps_city"
 
-#' ZIP code regex
-#'
-#' The regex string to extract 5 or 9 digit zip from end of address.
-#'
-#' @format A single character string.
-"rx_zip"
+#' @title USPS State Abbreviations
+#' @description A tibble containing the USPS.
+#' @format A tibble with 62 rows of 2 variables:
+#' \describe{
+#'   \item{full}{Primary Street Suffix}
+#'   \item{abb}{Commonly Used Street Suffix or Abbreviation}
+#'   ...
+#' }
+#' @source USPS Appendix B,
+#'    [Two–Letter State Abbreviations](https://pe.usps.com/text/pub28/28apb.htm)
+"usps_state"
 
-#' State regex
-#'
-#' The regex string to extract alpha state string preceding ZIP code
-#'
-#' @format A single character string.
-"rx_state"
+#' @title US State Names
+#' @description The `state` column of the `usps_state` tibble.
+#' @details Contains 12 more names than [datasets::state.name].
+#' @format A vectory of state names (length 62).
+"valid_name"
 
-#' Many of the cities in America
-#'
-#' The `city` column of the `geo` tibble
-#'
-#' @format A vectory of city names
-"valid_city"
-
-#' All 2-letter State Abbreviations
-#'
-#' The `state` column of the `geo` tibble
-#'
-#' @format A vectory of 2-digit abbreviations
+#' @title US State Abbreviations
+#' @description The `abb` column of the `usps_state` tibble.
+#' @format A vector of 2-digit abbreviations (length 62).
 "valid_state"
 
-#' Almost all of the valid USA ZIP Codes
-#'
-#' The `zip` column of the `geo` tibble
-#'
-#' @format A vectory of 5-digit ZIP codes
+#' @title US City Names
+#' @description The `city` column of the `zipcodes` tibble.
+#' @format A sorted vector of unique city names (length 19,083).
+"valid_city"
+
+#' @title Invalid City Names
+#' @description A custom vector containing common invalid city names.
+#' @format A vector of length 54.
+"invalid_city"
+
+#' @title Almost all of the valid USA ZIP Codes
+#' @description The `zip` column of the `geo` tibble.
+#' @format A sorted vector of 5-digit ZIP codes (length 44334).
 "valid_zip"
+
+#' @title ZIP code regex
+#' @description The regex string to extract ZIP code from the end of address.
+#' @format A character string (length 1).
+"rx_zip"
+
+#' @title State regex
+#' @description The regex string to extract state string preceding ZIP code.
+#' @format A character string (length 1).
+"rx_state"
