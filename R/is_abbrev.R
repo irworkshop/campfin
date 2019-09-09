0b2766438f23e254081499ce31a6d3a5ee36524b
Kiernan Nicholls <kiernann@protonmail.com>
2019-09-09 17:11:58 -0400

    Change is_abbrev to nested ifs

--- R/is_abbrev.R
+++ R/is_abbrev.R
@@ -6,39 +6,26 @@
 #' @param abb A suspected abbreviation
 #' @param full A long form string to test against
 #' @return logical; whether `abb` is potential abbreviation of `full`
-#' @importFrom stringr str_split
+#' @importFrom stringr str_split str_to_lower
 #' @importFrom dplyr lead
 #' @examples
 #' is_abbrev(abb = "BRX", full = "BRONX")
 #' is_abbrev(abb = state.abb, full = state.name)
 #' is_abbrev(abb = "NOLA", full = "New Orleans")
 #' @export
-is_abbrev <- Vectorize(
-  function(abb, full) {
-    less <- nchar(abb) < nchar(full)
-    match <- .first_letter_match(x = abb, y = full)
-    inside <- .letters_inside(x = abb, y = full)
-    ordered <- .letters_ordered(x = abb, y = full)
-    all <- less & match & inside & ordered
-    return(unname(all))
-  }, USE.NAMES = FALSE
-)
-
-.first_letter_match <- function(x, y) {
-    stringr::str_sub(x, end = 1) == stringr::str_sub(y, end = 1)
-}
-
-.letters_inside <- function(x, y) {
-    split_x <- stringr::str_split(stringr::str_to_lower(x), pattern = "", simplify = TRUE)
-    split_y <- stringr::str_split(stringr::str_to_lower(y), pattern = "", simplify = TRUE)
-    all(split_x %in% split_y)
-}
-
-.letters_ordered <- function(x, y) {
-    xs <- stringr::str_split(x, pattern = "", simplify = TRUE)
-    xs <- stringr::str_to_lower(xs)
-    ys <- stringr::str_split(y, pattern = "", simplify = TRUE)
-    ys <- stringr::str_to_lower(ys)
-    match_order <- match(xs, ys)
-    all(match_order < dplyr::lead(match_order), na.rm = TRUE)
+is_abbrev <- function(abb, full) {
+  abb  <- stringr::str_split(stringr::str_to_lower(abb),  "")[[1]]
+  full <- stringr::str_split(stringr::str_to_lower(full), "")[[1]]
+  if (length(abb) >= length(full)) {
+    return(FALSE)
+  } else {
+    if (abb[[1]] != full[[1]]) {
+      return(FALSE)
+    } else {
+      inside <- all(abb %in% full)
+      order <- match(abb, full)
+      ordered <- all(order < dplyr::lead(order), na.rm = TRUE)
+      return(all(inside, ordered))
+    }
+  }
 }
