46005eac41ab50d89cbbf991d7df32752c1da7f7
Kiernan Nicholls <kiernann@protonmail.com>
2019-09-25 19:18:19 -0400

    Save keypad letter and number named vectors

--- data-raw/data_keypad.R
+++ data-raw/data_keypad.R
@@ -2,13 +2,17 @@ library(stringr)
 library(usethis)
 
 # create vector for numbers per letter
-keypad <- as.character(c(rep(2:6, each = 3), rep(7, 4), rep(8, 3), rep(9, 4)))
-length(keypad) == 26
+numbers <- c(0, rep(2:6, each = 3), rep(7, 4), rep(8, 3), rep(9, 4))
+length(numbers) == 27
 # assign letter names to each number
-names(keypad) <- LETTERS
+names(numbers) <- c(" ", LETTERS)
+
+keypad_alpha <- setNames(as.character(numbers), c(" ", LETTERS))
+keypad_nums <- invert_named(keypad_alpha)
 
 # test stringr replace
-str_replace_all("TEST", keypad)
+str_replace_all("TEST", keypad_alpha)
+str_replace_all("8378", keypad_nums)
 
 # save keypad data internally
 use_data(keypad, internal = TRUE)
