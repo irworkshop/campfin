ff9417cc47c7f0d0231273e33cbcbcf56604c1e4
Kiernan Nicholls <kiernann@protonmail.com>
2019-09-25 22:04:12 -0400

    Remove keypad_*() from export

--- R/keypad_convert.R
+++ R/keypad_convert.R
@@ -2,8 +2,6 @@
 #' @param letter A character vector of letters.
 #' @return A character vector of numbers.
 #' @importFrom stringr str_replace_all str_to_upper
-#' @examples
-#' keypad_number(letter = "Example")
 keypad_number <- function(letter) {
   stringr::str_replace_all(stringr::str_to_upper(letter), keypad)
 }
@@ -11,8 +9,6 @@ keypad_number <- function(letter) {
 #' @title Return the Keypad Letters for a Number
 #' @param number A _single_ number.
 #' @return The numbers which would return `number` in [keypad_number()].
-#' @examples
-#' keypad_letters(number = 8)
 keypad_letters <- function(number) {
   keypad2 <- invert_named(keypad)
   unname(keypad2[which(names(keypad2) == number)])
