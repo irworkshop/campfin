fee6b54e4d3e74447c490cbac71fa08cd57fba9f
Yanqi Xu <yanqixu@outlook.com>
2019-11-11 16:40:48 -0500

    Rewrite check_city output to keep execution before warning/stop. fetch_city now returns the full formatted address.

--- R/check_city.R
+++ R/check_city.R
@@ -1,15 +1,19 @@
 #' @title Check whether an input is a valid place with Google Maps API
-#' @description Check whether a place is a valid place or misspelling by matching against
-#' the Google Geocoding seaerch result. Use the [httr::GET()] to
-#' send a request to the Google Maps API for geocoding information. The query will concatenate
-#' all the geographical information that is passed in into a long string.
-#' Then the function pulls the `formatted_address` endpoint of the API results
-#' and then identifies and extracts the long name field from the API _locality_ result and compare
-#' it against the input to see if the input and output match up.
-#' Note that you will need to pass in your Google Maps Place API key with the arguement `key=`.
-#' @param city A string of city name to be submitted to the Google Maps Geocode API and tested.
-#' @param state Optional. The state associated with the city that you wish to check against the Maps API.
-#' @param zip Optional. Supply a string of zipcode will help with matching precision
+#' @description Check whether a place is a valid place or misspelling by
+#'   matching against the Google Geocoding seaerch result. Use the [httr::GET()]
+#'   to send a request to the Google Maps API for geocoding information. The
+#'   query will concatenate all the geographical information that is passed in
+#'   into a long string. Then the function pulls the `formatted_address`
+#'   endpoint of the API results and then identifies and extracts the long name
+#'   field from the API _locality_ result and compare it against the input to
+#'   see if the input and output match up. Note that you will need to pass in
+#'   your Google Maps Place API key with the arguement `key=`.
+#' @param city A string of city name to be submitted to the Google Maps Geocode
+#'   API and tested.
+#' @param state Optional. The state associated with the city that you wish to
+#'   check against the Maps API.
+#' @param zip Optional. Supply a string of zipcode will help with matching
+#'   precision
 #' @param key a character string to be passed into key=''
 #' @param guess a logical vector. If true, the function returns a dataframe with
 #'   three columns. In addition to the first logical vector, it would return a
@@ -19,10 +23,12 @@
 #'   is set to TRUE, the results returned can be any address, including
 #'   highways, street addresses, cities, townships, states, provinces,
 #'   countires, etc.
-#' @return By default, returns a logical vector: If the city returned by the API comes
-#' back the same as the city input, the function will evaluate to true;
-#' otherwise it will evaluate to false.The evaluation ignores case.
-#' If the the guess argument is set to T, returns a dataframe with a logical vector in the first column and the normalized address in the second.
+#' @return By default, returns a logical vector: If the city returned by the API
+#'   comes back the same as the city input, the function will evaluate to true;
+#'   otherwise it will evaluate to false.The evaluation ignores case. If the the
+#'   guess argument is set to T, returns a dataframe with columns
+#'   `original_city`, `original_state`, `original_zip`, `check_city_flag`,
+#'   `guess_city` and `guess_place`.
 #' @examples
 #' \dontrun{ requires Google API key
 #' check_city("WYOMISSING", "PA", key = your_key) #replace your_key with your API key
@@ -56,8 +62,8 @@ check_city <- function(city = NULL, state = NULL, zip = NULL, key = NULL, guess 
   }
 
   if(city == ""|is.na(city)){
-    create_table()
     stop("Input is empty", call. = FALSE)
+    create_table()
   }
   api_url_root <- "https://maps.googleapis.com/maps/api/geocode/json?"
   response <- httr::GET(api_url_root,
@@ -66,16 +72,16 @@ check_city <- function(city = NULL, state = NULL, zip = NULL, key = NULL, guess 
   stop_for_status(response)
   r_content <- content(response)
   if (identical(r_content$status,   "REQUEST_DENIED")) {
+    warning("You must use an API key", call. = FALSE)
     create_table()
-    stop("You must use an API key", call. = FALSE)
   } else if (identical(r_content$status,   "ZERO_RESULTS")) {
     city_validity = FALSE
+    warning(str_c("No results were found for ", city, " ", state, " ", zip))
     create_table()
-    stop("No results were found")
   } else if(r_content$status != 'OK'){
+    warning(glue("Error from API: {r_content$status}, see Google Maps Documentation for details"),
+            call. = FALSE)
     create_table()
-    stop(glue("Error from API: {r_content$status}, see Google Maps Documentation for details"),
-         call. = FALSE)
   } else {
     returned_address <- r_content$results[[1]]$formatted_address %>% str_to_upper()
     returned_city <- str_match(returned_address,"(^.[^,]+),\\s.+")[,2]
@@ -97,4 +103,3 @@ check_city <- function(city = NULL, state = NULL, zip = NULL, key = NULL, guess 
     create_table()
   }
 }
-
--- R/fetch_city.R
+++ R/fetch_city.R
@@ -7,13 +7,12 @@
 #' and then identifies and extracts the the first field of the result. Note that you will
 #' need to pass in your Google Maps Place API key with the arguement `key=`.
 #' @param address A vector of street addresses. Sent to the API all in one string.
-#' @param city A string of city name to be submitted to the Google Maps Geocode API.
-#' @param state Optional. The state associated with the city that you wish to check against the Maps API.
 #' @param key a character string to be passed into key=''. This is your API key obtained from Google.
-#' @return A character vector: results of the closest match of the submitted city names returned by the API.
+#' @return A character vector: results of the formatted address endpoint from Google. This will include all the fields
+#' from street address, city, state/province, zipcode/postal code to country/regions.
 #' @examples
 #' \dontrun{ requires Google API key
-#' fetch_city("One CVS Drive","RI", "02895", key = api_key)
+#' fetch_city("4400 Mass Ave. DC", key = api_key) # Use your API key from Google Maps
 #' }
 #' @seealso \url{http://code.google.com/apis/maps/documentation/geocoding/}
 #' @importFrom httr GET stop_for_status
@@ -23,36 +22,30 @@
 #' @importFrom dplyr if_else
 #' @family geographic normalization functions
 #' @export
-fetch_city <- function(address = NULL, city = NULL, state = NULL, key = NULL) {
-  if(str_c(address,city,state) %>% trimws() == ""|is.na(str_c(address,city,state))){
-    return(NA_character_)
+fetch_city <- function(address = NULL, key = NULL) {
+  if(address %>% trimws() == ""|is.na(address)){
     stop("geographical arguments required")
+    return(NA_character_)
   }
   api_url_root <- "https://maps.googleapis.com/maps/api/geocode/json?"
-  response <- httr::GET(api_url_root, query = list(address = str_c(address, city, state, sep = "+"), key = key))
+  response <- httr::GET(api_url_root, query = list(address = address, key = key))
   stop_for_status(response)
   r_content <- content(response)
   if (identical(r_content$status,   "REQUEST_DENIED")) {
+    warning("You must use an API key", call. = FALSE)
     return(NA_character_)
-    stop("You must use an API key", call. = FALSE)
   }
   else if (identical(r_content$status,   "ZERO_RESULTS")){
+    warning("No results were found", call. = FALSE)
     return(NA_character_)
-    stop("No results were found", call. = FALSE)
   }
   else if(r_content$status != 'OK'){
-    stop(glue("Error from API: {r_content$status}, see Google Maps Documentation for details"), call. = FALSE)
+    warning(glue("Error from API: {r_content$status}, see Google Maps Documentation for details"), call. = FALSE)
     return(NA_character_)
   }
    else{
      address_list <- r_content$results[[1]]$address_components
      returned_address <- r_content$results[[1]]$formatted_address %>% str_to_upper()
-     returned_city <- str_match(returned_address,"(^.[^,]+),\\s.+")[,2]
-     normal_returned <- normal_city(city = returned_city,
-                                    geo_abbs = campfin::usps_city,
-                                    st_abbs = c(campfin::valid_state),
-                                    na = campfin::invalid_city,
-                                    na_rep = TRUE)
-    return(normal_returned)
+    return(returned_address)
   }
 }
