a4f462f6076796d21befbe2a7b2ae46255280a9c
Kiernan Nicholls <kiernann@protonmail.com>
2019-11-12 17:05:48 -0500

    Tweak url2path() doc style

--- R/url2path.R
+++ R/url2path.R
@@ -1,8 +1,9 @@
-#' @title Make a File Path from a URL
-#' @details Useful in the `destfile` argument to [download.file()] to save a
-#'   file with the same name as the URL's file name.
-#' @description Combine the [basename()] of a file URL with the path to a
-#'   directory.
+#' Make a File Path from a URL
+#'
+#' Useful in the `destfile` argument to [download.file()] to save a file with
+#' the same name as the URL's file name.
+#'
+#' @description Combine the [basename()] of a file URL with a directory path.
 #' @param url The URL of a file to download.
 #' @param dir The directory where the file will be downloaded.
 #' @return The desired file path to a URL file.
