3c1aae817257f5afafc723817e9de768d35f2b7e
Kiernan Nicholls <kiernann@protonmail.com>
2019-11-12 15:45:03 -0500

    Tweak flag_*() styles

--- R/flag-dupes.R
+++ R/flag-dupes.R
@@ -1,19 +1,18 @@
-#' @title Flag Duplicate Rows With New Column
-#' @description This function uses [dplyr::mutate()] to create a new `dupe_flag`
-#'   logical variable with `TRUE` values for any record duplicated more than
-#'   once.
+#' Flag Duplicate Rows With New Column
+#'
+#' This function uses [dplyr::mutate()] to create a new `dupe_flag` logical
+#' variable with `TRUE` values for any record duplicated more than once.
+#'
 #' @param data A data frame to flag.
-#' @param  ... Arguments passed to [dplyr::select()] (needs to be at least
+#' @param ... Arguments passed to [dplyr::select()] (needs to be at least
 #'   [dplyr::everything()]).
 #' @return A data frame with a new `dupe_flag` logical variable.
 #' @importFrom dplyr mutate
 #' @examples
 #' flag_dupes(iris, dplyr::everything())
 #' @export
-  flag_dupes <- function(data, ...) {
-    sub_data <- dplyr::select(data, ...)
-    dupe_vec <- duplicated(sub_data)
-    flagged_df <- dplyr::mutate(data, dupe_flag = dupe_vec)
-    rm(sub_data, dupe_vec)
-    flagged_df
-  }
+flag_dupes <- function(data, ...) {
+  sub_data <- dplyr::select(data, ...)
+  dupe_vec <- duplicated(sub_data)
+  dplyr::mutate(data, dupe_flag = dupe_vec)
+}
