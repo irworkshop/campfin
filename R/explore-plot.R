a019588270df5fbdf26bb5a2cdb1e48e18e97ba0
Kiernan Nicholls <kiernann@protonmail.com>
2019-11-12 14:18:55 -0500

    Tweak explore_plot() style

--- R/explore-plot.R
+++ R/explore-plot.R
@@ -1,17 +1,22 @@
-#' @title Create Basic Barplots
-#' @description This function simply wraps around [ggplot2::geom_col()] to take
-#'   a dataframe and categorical variable to return a simple barplot count.
+#' Create Basic Barplots
+#'
+#' This function simply wraps around [ggplot2::geom_col()] to take a dataframe
+#' and categorical variable to return a custom barplot `ggplot` object. The bars
+#' are arranged in descending order and are limited to the 8 most frequent
+#' values.
+#'
 #' @param data The data frame to explore.
 #' @param var A variable to plot.
 #' @param flip logical; Whether plot should be flipped horizontally.
 #' @param nbar The number of bars to plot. Always shows most common values.
 #' @param palette The color palette passed to [ggplot2::scale_fill_brewer().
-#' @param ... Optional arguments passed to [ggplot2::labs()].
-#' @return A bar plot.
+#' @param ... Additional optional arguments passed to [ggplot2::labs()].
+#' @return A `ggplot` barplot object. Can then be combined with other `ggplot`
+#'   layers with `+` to customize.
 #' @examples
 #' explore_plot(ggplot2::diamonds, cut)
-#' @importFrom ggplot2 ggplot geom_col scale_fill_brewer scale_y_continuous aes
 #' @importFrom dplyr count mutate desc
+#' @importFrom ggplot2 ggplot geom_col scale_fill_brewer scale_y_continuous aes
 #' @importFrom stringr str_to_title str_replace_all
 #' @export
 explore_plot <- function(data, var, flip = FALSE, nbar = 8, palette = "Dark2", ...) {
@@ -30,10 +35,9 @@ explore_plot <- function(data, var, flip = FALSE, nbar = 8, palette = "Dark2", .
       y = "Percent",
       ...
     )
-  flip_plot <- base_plot + ggplot2::coord_flip()
   if (!flip) {
-    return(base_plot)
+    base_plot + ggplot2::coord_flip()
   } else {
-    return(flip_plot)
+    base_plot
   }
 }
