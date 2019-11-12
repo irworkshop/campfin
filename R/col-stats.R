ddd6fe95a4762cc2d65fd4632f51aa189d72776b
Kiernan Nicholls <kiernann@protonmail.com>
2019-11-12 10:17:31 -0500

    Deprecate glimpse_fun() for col_stats()
    Use rlang::as_label() instead of importing pillar.

--- R/col-stats.R
+++ R/col-stats.R
@@ -1,4 +1,4 @@
-#' @title Glimpse Column Count Functions
+#' @title Apply a statistic function to all column vectors
 #' @description Apply a counting summary function like [dplyr::n_distinct()] or
 #'   [count_na()] to every column of a dataframe and return the results along
 #'   with a _percentage_ of that value.
@@ -8,21 +8,29 @@
 #' @return A tibble with a row for every column and new columns with count and
 #'   proportion.
 #' @examples
-#' glimpse_fun(dplyr::storms, dplyr::n_distinct)
-#' glimpse_fun(dplyr::storms, campfin::count_na)
+#' col_stats(dplyr::storms, dplyr::n_distinct)
+#' col_stats(dplyr::storms, campfin::count_na)
 #' @importFrom purrr map
 #' @importFrom dplyr mutate select
 #' @importFrom tibble enframe
 #' @importFrom rlang as_label
 #' @export
-glimpse_fun <- function(data, fun, print = TRUE) {
-  .Deprecated("col_stats")
-  summary <- data %>%
-    purrr::map_dbl({{ fun }}) %>%
+col_stats <- function(data, fun, print = TRUE) {
+  if (class(fun) != "function") {
+    stop("The fun argument must be a function.")
+  }
+  if ("data.frame" %out% class(data)) {
+    stop("The data argument must be a data frame or similar.")
+  }
+  stats <- unlist(purrr::map(data, fun))
+  if (!is.numeric(stats) | length(stats) != ncol(data)) {
+    stop("The return of fun must be numeric length one.")
+  }
+  summary <- stats %>%
     tibble::enframe(name = "col", value = "n") %>%
     dplyr::mutate(p = .data$n / nrow(data)) %>%
-    dplyr::mutate(type = format(purrr::map(data, rlang::as_label))) %>%
-    dplyr::select(.data$col, .data$type, .data$n, .data$p)
+    dplyr::mutate(class = purrr::map_chr(data, rlang::as_label)) %>%
+    dplyr::select(.data$col, .data$class, .data$n, .data$p)
   if (print) {
     print(summary, n = length(data))
   }
