0f50d2ffe82b2279b961ffa0c9e737ef6a69c86d
Kiernan Nicholls <kiernann@protonmail.com>
2019-11-13 16:35:47 -0500

    Improve check_city() internal style

--- R/check-city.R
+++ R/check-city.R
@@ -1,42 +1,42 @@
-#' @title Check whether an input is a valid place with Google Maps API
-#' @description Check whether a place is a valid place or misspelling by
-#'   matching against the Google Geocoding seaerch result. Use the [httr::GET()]
-#'   to send a request to the Google Maps API for geocoding information. The
-#'   query will concatenate all the geographical information that is passed in
-#'   into a long string. Then the function pulls the `formatted_address`
-#'   endpoint of the API results and then identifies and extracts the long name
-#'   field from the API _locality_ result and compare it against the input to
-#'   see if the input and output match up. Note that you will need to pass in
-#'   your Google Maps Place API key with the arguement `key=`.
-#' @param city A string of city name to be submitted to the Google Maps Geocode
-#'   API and tested.
-#' @param state Optional. The state associated with the city that you wish to
-#'   check against the Maps API.
-#' @param zip Optional. Supply a string of zipcode will help with matching
-#'   precision
-#' @param key a character string to be passed into key=''
-#' @param guess a logical vector. If true, the function returns a dataframe with
-#'   three columns. In addition to the first logical vector, it would return a
-#'   `locality` column with the API-matched localities, i.e. unincorporated
-#'   cities or census designated places returned by API, and a third column
-#'   first part of matched formatted address returned by the API. When `place`
-#'   is set to TRUE, the results returned can be any address, including
-#'   highways, street addresses, cities, townships, states, provinces,
-#'   countires, etc.
-#' @return By default, returns a logical vector: If the city returned by the API
-#'   comes back the same as the city input, the function will evaluate to true;
-#'   otherwise it will evaluate to false.The evaluation ignores case. If the the
-#'   guess argument is set to T, returns a dataframe with columns
-#'   `original_city`, `original_state`, `original_zip`, `check_city_flag`,
-#'   `guess_city` and `guess_place`.
+#' Check whether an input is a valid place with Google Maps API
+#'
+#' Check whether a place is a valid place or misspelling by matching against the
+#' Google Geocoding seaerch result. Use the [httr::GET()] to send a request to
+#' the Google Maps API for geocoding information. The query will concatenate all
+#' the geographical information that is passed in into a long string. Then the
+#' function pulls the `formatted_address` endpoint of the API results and then
+#' identifies and extracts the long name field from the API _locality_ result
+#' and compare it against the input to see if the input and output match up.
+#' Note that you will need to pass in your Google Maps Place API key with the
+#' `key` arguement.
+#'
+#' @param city A string of city name to be submitted to the Geocode API.
+#' @param state Optional. The state associated with the `city`.
+#' @param zip Optional. Supply a string of ZIP code to increase precision.
+#' @param key A character string to be passed into `key`. Save your key as
+#'   "GEOCODE_KEY" using `Sys.setenv()` or by editing your `.Renviron` file.
+#' @param guess logical; Should the function return a single row tibble
+#'   containing the orignal data sent and the multi components returned by the
+#'   Geocode API.
+#' @return A logical value by default. If the city returned by the API
+#'   comes back the same as the city input, the function will evaluate to
+#'   `TRUE`, in all other circumstances (including API errors) `FALSE` is returned.
+#'
+#'   If the the `guess` argument is set to `TRUE`, a tibble with 1 row and six
+#'   columns is returned:
+#'   * `original_city`: The `city` value sent to the API.
+#'   * `original_state`: The `state` value sent to the API.
+#'   * `original_zip`: The `zip` value sent to the API.
+#'   * `check_city_flag`: logical; whether the guessed city matches.
+#'   * `guess_city`: The legal city guessed by the API.
+#'   * `guess_place`: The generic locality guessed by the API.
 #' @examples
-#' \dontrun{ requires Google API key
-#' check_city("WYOMISSING", "PA", key = your_key) #replace your_key with your API key
-#' check_city("Waggaman", "LA", "70094", key = your_key, guess = TRUE)
+#' \dontrun{requires Google API key
+#' check_city("WYOMISSING", "PA")
+#' check_city("Waggaman", "LA", "70094", guess = TRUE)
 #' }
 #' @seealso \url{http://code.google.com/apis/maps/documentation/geocoding/}
-#' @importFrom httr GET stop_for_status
-#' @importFrom httr content
+#' @importFrom httr GET stop_for_status content
 #' @importFrom stringr str_c str_match
 #' @importFrom dplyr if_else
 #' @importFrom purrr map_lgl
@@ -47,62 +47,76 @@ check_city <- function(city = NULL, state = NULL, zip = NULL, key = NULL, guess 
   city_validity = NA
   locality = NA_character_
   normal_returned = NA_character_
+  if (nchar(city) == 0 | is.na(city)) {
+    stop("Input is empty", call. = FALSE)
+    create_table()
+  }
+  if (is.null(key)) {
+    key <- Sys.getenv("GEOCODE_KEY")
+  }
+  api_url_root <- "https://maps.googleapis.com/maps/api/geocode/json?"
+  response <- httr::GET(
+    url = api_url_root,
+    query = list(
+      address = stringr::str_c(city, state, zip, sep = "+"),
+      key = key
+    )
+  )
+  httr::stop_for_status(response)
+  r_content <- httr::content(response)
   create_table <- function() {
     if (guess) {
       y <- tibble::tibble(
         original_city = city,
-        original_state = ifelse(is.null(state),NA_character_,state),
-        original_zip = ifelse(is.null(zip),NA_character_,zip),
+        original_state = ifelse(is.null(state), NA_character_, state),
+        original_zip = ifelse(is.null(zip), NA_character_, zip),
         check_city_flag = city_validity,
         guess_city = locality,
-        guess_place = normal_returned
+        guess_place = returned_norm
       )
       return(y)
     } else {
       return(city_validity)
     }
   }
-  if(city == ""|is.na(city)){
-    stop("Input is empty", call. = FALSE)
+
+  if (r_content$status == "REQUEST_DENIED") {
+    warning("You must use an API key")
     create_table()
-  }
-  api_url_root <- "https://maps.googleapis.com/maps/api/geocode/json?"
-  response <- httr::GET(api_url_root,
-                        query = list(address = str_c(city, state, zip, sep = "+"),
-                                     key = key))
-  stop_for_status(response)
-  r_content <- content(response)
-  if (identical(r_content$status,   "REQUEST_DENIED")) {
-    warning("You must use an API key", call. = FALSE)
+  } else if (r_content$status == "ZERO_RESULTS") {
+    warning(glue::glue("No results were found for {city}, {state}, {zip"))
+    city_validity <- FALSE
     create_table()
-  } else if (identical(r_content$status,   "ZERO_RESULTS")) {
-    city_validity = FALSE
-    warning(str_c("No results were found for ", city, " ", state, " ", zip))
-    create_table()
-  } else if(r_content$status != 'OK'){
-    warning(glue("Error from API: {r_content$status}, see Google Maps Documentation for details"),
-            call. = FALSE)
+  } else if(r_content$status != "OK"){
+    warning(glue("Error from API: {r_content$status}, see Google Maps Documentation for details"))
     create_table()
   } else {
-    returned_address <- r_content$results[[1]]$formatted_address %>% str_to_upper()
-    returned_city <- str_match(returned_address,"(^.[^,]+),\\s.+")[,2]
-    normal_returned <- normal_city(
+    returned_address <- stringr::str_to_upper(r_content$results[[1]]$formatted_address)
+    returned_city <- stringr::str_split(returned_address, ",\\s", simplify = TRUE)[, 1]
+    returned_norm <- normal_city(
       city = returned_city,
       abbs = campfin::usps_city,
       states = c(campfin::valid_state),
       na = campfin::invalid_city,
       na_rep = TRUE
     )
-    city_validity <- str_to_upper(city) %>% trimws() == normal_returned
+    city_validity <- grepl(city, returned_norm, ignore.case = TRUE)
     address_list <- r_content$results[[1]]$address_components
-    locality_position <- lapply(address_list, unlist,recursive = T) %>% map(str_detect, "locality") %>% map_lgl(any)
-    locality <- ifelse(TRUE %in% locality_position,
-                       address_list[locality_position][[1]]$long_name %>% normal_city(.,abbs = campfin::usps_city,
-                                                                                      states = c(campfin::valid_state),
-                                                                                      na = campfin::invalid_city,
-                                                                                      na_rep = TRUE),
-                       NA_character_
+    locality_position <- purrr::detect_index(
+      .x = purrr::transpose(address_list)$type,
+      .f = ~any(stringr::str_detect(., "locality"))
     )
+    locality <- if (any(locality_position)) {
+      normal_city(
+        city = address_list[[locality_position]]$long_name,
+        abbs = campfin::usps_city,
+        states = campfin::valid_state,
+        na = campfin::invalid_city,
+        na_rep = TRUE
+      )
+    } else {
+      NA_character_
+    }
     create_table()
   }
 }
