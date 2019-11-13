2c75cc666c42b5592469fdccf4db6125724c2d89
Kiernan Nicholls <kiernann@protonmail.com>
2019-11-13 14:19:53 -0500

    Test exploration plots

---
+++ tests/testthat/test-explore-plot.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+library(testthat)
+library(campfin)
+
+test_that("exploration plots are ggplot objects", {
+  plot <- explore_plot(diamonds, cut)
+  expect_s3_class(plot, "ggplot")
+})
+
+test_that("exploration plots can be flipped with an argument", {
+  plot <- explore_plot(diamonds, cut, flip = TRUE)
+  expect_s3_class(plot$coordinates, "CoordFlip")
+  plot <- explore_plot(diamonds, cut, flip = FALSE)
+  expect_true("CoordFlip" %out% class(plot$coordinates))
+})
