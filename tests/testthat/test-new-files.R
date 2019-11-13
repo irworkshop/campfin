f7aff71c674ac60d9f59d6bdf86131325cf154e4
Kiernan Nicholls <kiernann@protonmail.com>
2019-11-13 14:20:02 -0500

    Test file age checking

---
+++ tests/testthat/test-new-files.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(testthat)
+library(campfin)
+library(here)
+library(fs)
+
+dir_create(here("dir"))
+file_create(here("dir", "temp.txt"))
+
+test_that("the age of all files in a directory can be checked", {
+  expect_true(all_files_new("dir"))
+  expect_false(all_files_new(here("data")))
+})
+
+test_that("the age of a single file can be checked", {
+  expect_true(this_file_new(here("dir", "temp.txt")))
+  expect_false(this_file_new(here("README.md")))
+})
+
+dir_delete(here("dir"))
