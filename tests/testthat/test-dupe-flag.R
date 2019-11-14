954558eea8b1cc679bbcd27ab5724ecfa55ac35c
Kiernan Nicholls <kiernann@protonmail.com>
2019-11-14 10:26:43 -0500

    Test dupe flagging

---
+++ tests/testthat/test-dupe-flag.R
@@ -0,0 +1,23 @@
+library(testthat)
+library(campfin)
+library(tibble)
+library(dplyr)
+
+test_that("a new logical dupe_flag variable can be added", {
+  df <- tibble(x = c("a", "b", "c", "c"), y = c("1", "2", "3", "3"))
+  df <- flag_dupes(df, everything())
+  expect_type(df$dupe_flag, "logical")
+  expect_equal(sum(df$dupe_flag), 1)
+})
+
+test_that("columns can be ignored when flagging dupes", {
+  df <- tibble(
+    id = 1:4,
+    x = c("a", "b", "c", "c"),
+    y = c("1", "2", "3", "3")
+  )
+  df <- flag_dupes(df, -id)
+  expect_equal(n_distinct(df$id), 4)
+  expect_type(df$dupe_flag, "logical")
+  expect_equal(sum(df$dupe_flag), 1)
+})
