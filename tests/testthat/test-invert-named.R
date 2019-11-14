b405a6a9896ca7a4ab68ef7a6a7426e094457934
Kiernan Nicholls <kiernann@protonmail.com>
2019-11-14 10:34:21 -0500

    Test named vector inversion

---
+++ tests/testthat/test-invert-named.R
@@ -0,0 +1,8 @@
+library(testthat)
+library(campfin)
+
+test_that("names and values of a vector can be inverted", {
+  x <- invert_named(c(name = "value"))
+  expect_equal(names(x), "value")
+  expect_equal(unname(x), "name")
+})
