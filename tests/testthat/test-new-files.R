5a1e6ef7acc536e3f40a33f89884b16aee0b6298
Kiernan Nicholls <kiernann@protonmail.com>
2019-11-18 14:59:32 -0500

    Test new file warnings

--- tests/testthat/test-new-files.R
+++ tests/testthat/test-new-files.R
@@ -3,15 +3,19 @@ library(campfin)
 library(fs)
 
 dir_create("dir")
+
+test_that("empty directories warn and return false", {
+  expect_false(expect_warning(all_files_new("dir")))
+})
+
+test_that("non-existant files warn and return false", {
+  expect_false(expect_warning(this_file_new("dir/temp.txt")))
+})
+
 file_create("dir/temp.txt")
 
 test_that("the age of all files in a directory can be checked", {
   expect_true(all_files_new("dir"))
-})
-
-test_that("the age of files in an old directory returns false", {
-  skip("directory path trouble running test suite")
-  expect_false(all_files_new("../../data/"))
 })
 
 test_that("the age of a single file can be checked", {
