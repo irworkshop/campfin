7eeffc873b87e4cb78d141ab1d907d8f987b46a8
Kiernan Nicholls <kiernann@protonmail.com>
2019-11-18 11:36:40 -0500

    Test URL file size check/format

---
+++ tests/testthat/test-url-file-size.R
@@ -0,0 +1,8 @@
+library(testthat)
+library(campfin)
+
+test_that("the file size of the URL can be checked from HEAD", {
+  url <- "https://raw.githubusercontent.com/irworkshop/campfin/master/README.md"
+  expect_type(url_file_size(url), "double")
+  expect_type(url_file_size(url, format = TRUE), "character")
+})
