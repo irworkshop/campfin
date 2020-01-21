e01093a784f67a6fc79024b95aed7771e2eebec9
Kiernan Nicholls <kiernann@protonmail.com>
2020-01-21 15:32:14 -0500

    Allow toggle str_normal() steps

--- R/str-normal.R
+++ R/str-normal.R
@@ -9,17 +9,33 @@
 #'   4. Trim and squish excess whitespace.
 #'
 #' @param x A character string to normalize.
+#' @param case logical; whether [stringr::str_to_upper()] should be called.
+#' @param punct logical; whether [stringr::str_replace_all()] should be called on puctuation.
+#' @param quote logical; whether [stringr::str_replace_all()] should be called on double quotes.
+#' @param trim logical; whether [stringr::str_trim()] should be called.
+#' @param squish logical; whether [stringr::str_squish()] should be called.
 #' @return A normalized vector of the same length.
 #' @examples
 #' str_normal("   TestING 123   example_test.String   ")
 #' @importFrom stringr str_to_upper str_replace_all str_trim str_squish
 #' @family geographic normalization functions
 #' @export
-str_normal <- function(x) {
-  x %>%
-    stringr::str_to_upper() %>%
-    stringr::str_replace_all("[[:punct:]]", " ") %>%
-    stringr::str_replace_all("\"", "\'") %>%
-    stringr::str_trim() %>%
-    stringr::str_squish()
+str_normal <- function(x, case = TRUE, punct = TRUE, quote = TRUE, trim = TRUE, squish = TRUE) {
+  if (case) {
+    x <- stringr::str_to_upper(x)
+  }
+  if (punct) {
+    x <- stringr::str_replace_all(x, "[[:punct:]]", " ")
+  }
+  if (quote) {
+    x <- stringr::str_replace_all(x, "\"", "\'")
+  }
+  if (trim) {
+    x <- stringr::str_trim(x)
+  }
+  if (squish) {
+    x <- stringr::str_squish(x)
+  }
+  return(x)
 }
+
