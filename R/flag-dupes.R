e7ad02b165ecb591a06f2669ce0b6210539dfe28
Kiernan Nicholls <kiernann@protonmail.com>
2020-01-22 15:04:38 -0500

    Auto remove no dupe col

--- R/flag-dupes.R
+++ R/flag-dupes.R
@@ -6,13 +6,20 @@
 #' @param data A data frame to flag.
 #' @param ... Arguments passed to [dplyr::select()] (needs to be at least
 #'   [dplyr::everything()]).
+#' @param .check Whether the resulting column should be summed and
+#'   removed if empty.
 #' @return A data frame with a new `dupe_flag` logical variable.
 #' @importFrom dplyr mutate
 #' @examples
 #' flag_dupes(iris, dplyr::everything())
 #' @export
-flag_dupes <- function(data, ...) {
+flag_dupes <- function(data, ..., .check = TRUE) {
   sub_data <- dplyr::select(data, ...)
   dupe_vec <- duplicated(sub_data)
-  dplyr::mutate(data, dupe_flag = dupe_vec)
+  if (.check & sum(dupe_vec) == 0) {
+    warning("no duplicate rows, column not created")
+    return(data)
+  } else {
+    dplyr::mutate(data, dupe_flag = dupe_vec)
+  }
 }
