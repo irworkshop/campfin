66f7481bdffb74e72ee1885a9c1807434f80c793
Kiernan Nicholls <kiernann@protonmail.com>
2020-01-29 12:33:08 -0500

    Use fs paths and bytes

--- R/url-file-size.R
+++ R/url-file-size.R
@@ -4,18 +4,14 @@
 #' downloaded.
 #'
 #' @param url The URL of the file to query.
-#' @param format logical; Whether to format as byte measurement and symbol.
 #' @return The size of a file to be downloaded.
 #' @importFrom httr HEAD headers
-#' @importFrom scales number_bytes
+#' @importFrom fs as_fs_bytes
 #' @examples
 #' url_file_size("https://campaignfinance.cdn.sos.ca.gov/dbwebexport.zip", format = TRUE)
 #' @export
-url_file_size <- function(url, format = FALSE) {
+url_file_size <- function(url) {
   file_head <- httr::HEAD(url = url)
   file_length <- as.numeric(httr::headers(file_head)[["content-length"]])
-  if (format) {
-    file_length <- scales::number_bytes(file_length)
-  }
-  return(file_length)
+  fs::as_fs_bytes(file_length)
 }
