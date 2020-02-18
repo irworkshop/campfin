a50006fdc3098347dd9b1bba599022871bfe288f
Kiernan Nicholls <kiernann@protonmail.com>
2020-02-18 12:35:19 -0500

    Add .both arg to flag_dupes()

--- R/flag-dupes.R
+++ R/flag-dupes.R
@@ -8,14 +8,20 @@
 #'   [dplyr::everything()]).
 #' @param .check Whether the resulting column should be summed and
 #'   removed if empty.
+#' @param .both Whether to flag both duplicates or just subsequent.
 #' @return A data frame with a new `dupe_flag` logical variable.
 #' @importFrom dplyr mutate
 #' @examples
 #' flag_dupes(iris, dplyr::everything())
+#' flag_dupes(iris, dplyr::everything(), .both = FALSE)
 #' @export
-flag_dupes <- function(data, ..., .check = TRUE) {
+flag_dupes <- function(data, ..., .check = TRUE, .both = TRUE) {
   sub_data <- dplyr::select(data, ...)
-  dupe_vec <- duplicated(sub_data) | duplicated(sub_data, fromLast = TRUE)
+  dupe_vec <- if (.both) {
+    duplicated(sub_data) | duplicated(sub_data, fromLast = TRUE)
+  } else {
+    duplicated(sub_data)
+  }
   if (.check & sum(dupe_vec) == 0) {
     warning("no duplicate rows, column not created")
     return(data)
