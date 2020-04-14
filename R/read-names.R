17b21d9c04cd3461bc301ad398a06bb4bf14cc1d
Kiernan Nicholls <kiernann@protonmail.com>
2020-04-14 12:08:24 -0400

    Create read_names()

---
+++ R/read-names.R
@@ -0,0 +1,14 @@
+#' Read column names
+#'
+#' Read the first line of a delimited file as vector.
+#'
+#' @param file Path to text file.
+#' @param delim Character separating column names.
+#' @return Character vector of column names.
+#' @examples
+#' read_names("x\n11/09/2016")
+#' @importFrom readr read_delim
+#' @export
+read_names <- function(file, delim = ",") {
+  names(readr::read_delim(file, delim, n_max = 0))
+}
