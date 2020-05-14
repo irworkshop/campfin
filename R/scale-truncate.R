41cebc756180556a039b85d709cf6bef1d7e4dd6
Kiernan Nicholls <kiernann@protonmail.com>
2020-05-14 13:13:47 -0400

    Create x-axis truncator

---
+++ R/scale-truncate.R
@@ -0,0 +1,13 @@
+#' Truncate x-axis labels
+#'
+#' Truncate the labels of a plot's discrete x-axis labels so that the text does
+#' not overflow and collide with other bars.
+#'
+#' @param n The maximum width of string passed to [stringr::str_trunc()].
+#' @param ... Additional arguments passed to [ggplot2::scale_x_discrete()].
+#' @importFrom ggplot2 scale_x_discrete
+#' @importFrom stringr str_trunc
+#' @export
+scale_x_truncate <- function(n = 20, ...) {
+   ggplot2::scale_x_discrete(..., label = function(x) stringr::str_trunc(x, 20))
+}
