0227e4dd4c34959c9805696c7c4b143799dd8400
Kiernan Nicholls <kiernann@protonmail.com>
2020-05-15 10:40:54 -0400

    Create path.abbrev()

---
+++ R/path-abbrev.R
@@ -0,0 +1,16 @@
+#' Abbreviate a file path
+#'
+#' This is an inverse of [path.expand()], which replaces the home directory or
+#' project directory with a tilde.
+#'
+#' @param path Character vector containing one or more full paths.
+#' @return Abbreviated file paths.
+#' @examples
+#' print(here::here())
+#' path.abbrev(here::here("test"))
+#' @importFrom stringr str_replace
+#' @importFrom here here
+#' @export
+path.abbrev <- function(path, dir = here::here()) {
+  stringr::str_replace(path, dir, "~")
+}
