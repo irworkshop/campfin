aeb2e0969df34f0f8f81248291dc9dc88b5fbbfa
Kiernan Nicholls <kiernann@protonmail.com>
2020-05-27 13:06:09 -0400

    Add cumsum to add_prop()

--- R/add-prop.R
+++ R/add-prop.R
@@ -5,11 +5,17 @@
 #' @details `mean(x %in% y)`
 #' @param .data A data frame with a count column.
 #' @param n The column name with a count, usually `n` from [dplyr::count()].
+#' @param sum Should [cumsum()] be called on the new `p` column.
 #' @return A data frame with the new column `p`.
 #' @examples
 #' add_prop(dplyr::count(ggplot2::diamonds, cut))
 #' @importFrom dplyr mutate
 #' @export
-add_prop <- function(.data, n) {
-  dplyr::mutate(.data, p = prop.table(n))
+add_prop <- function(.data, n, sum = FALSE) {
+  p <- if (sum) {
+    cumsum(prop.table(.data$n))
+  } else {
+    prop.table(.data$n)
+  }
+  dplyr::mutate(.data, p)
 }
