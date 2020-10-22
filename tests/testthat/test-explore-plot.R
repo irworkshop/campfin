14c3cbd1a2ce6c570738234e704ef85b02077e27
Kiernan Nicholls <kiernann@protonmail.com>
2020-10-22 11:05:15 -0400

    Remove flip test for plot

--- tests/testthat/test-explore-plot.R
+++ tests/testthat/test-explore-plot.R
@@ -6,10 +6,3 @@ test_that("exploration plots are ggplot objects", {
   plot <- explore_plot(diamonds, cut)
   expect_s3_class(plot, "ggplot")
 })
-
-test_that("exploration plots can be flipped with an argument", {
-  plot <- explore_plot(diamonds, cut, flip = TRUE)
-  expect_s3_class(plot$coordinates, "CoordFlip")
-  plot <- explore_plot(diamonds, cut, flip = FALSE)
-  expect_true("CoordFlip" %out% class(plot$coordinates))
-})
