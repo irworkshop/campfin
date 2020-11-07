9917298f6bc76c572b5a9e59c2c753cc02d3145e
Kiernan Nicholls <kiernann@protonmail.com>
2020-11-07 12:44:09 -0500

    Improve file download tests

--- tests/testthat/test-url-path.R
+++ tests/testthat/test-url-path.R
@@ -3,10 +3,11 @@ library(campfin)
 library(fs)
 
 test_that("file paths can be created from url basenames", {
+  skip_if_offline()
   url <- "https://raw.githubusercontent.com/irworkshop/campfin/master/README.md"
   dir_create("dir")
   path <- url2path(url, "dir")
-  download.file(url, path)
+  download.file(url, path, quiet = TRUE)
   expect_true(file_exists("dir/README.md"))
   dir_delete("dir")
 })
