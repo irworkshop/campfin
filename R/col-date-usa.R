1866aab500ede6bc8d3abf78d5c3d62de3fd4ce3
Kiernan Nicholls <kiernann@protonmail.com>
2021-01-15 16:04:14 -0500

    Deprecate col_date_usa and create col_date_mdy

--- R/col-date-usa.R
+++ R/col-date-usa.R
@@ -1,14 +1,23 @@
 #' Parse USA date columns in readr functions
 #'
 #' Parse dates with format MM/DD/YYYY. This function simply wraps around
-#' [readr::col_date()] with the `format` argument set to `"%m/%d/%Y"`. Most US
+#' [readr::col_date()] with the `format` argument set to `"%m/%d/%Y"`. Many US
 #' campaign finance datasets use this format.
 #'
+#' @param time An optional character vector specifying the time format.
 #' @return A `POSIXct` vector.
 #' @importFrom readr col_date cols
 #' @examples
-#' readr::read_csv(file = "x\n11/09/2016", col_types = readr::cols(x = col_date_usa()))
+#' readr::read_csv(file = "x\n11/09/2016", col_types = readr::cols(x = col_date_mdy()))
+#' @export
+col_date_mdy <- function(time = NULL) {
+  time <- ifelse(is.null(time), "", time)
+  readr::col_date(format = paste0("%m/%d/%Y", time))
+}
+
+#' @rdname col_date_mdy
 #' @export
 col_date_usa <- function() {
+  .Deprecated("col_date_mdy")
   readr::col_date(format = "%m/%d/%Y")
 }
