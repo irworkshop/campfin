108f2d4b6f05cd3fc8b55690e67db3fa68ede57a
Kiernan Nicholls <kiernann@protonmail.com>
2021-01-19 11:29:39 -0500

    flush_memory() defaults to 1 not 10

--- R/flush-memory.R
+++ R/flush-memory.R
@@ -4,8 +4,8 @@
 #'
 #' @param n The number of times to run [gc()].
 #' @export
-flush_memory <- function(n = 10) {
-  for (i in 1:n) {
+flush_memory <- function(n = 1) {
+  for (i in seq(n)) {
     suppressMessages(gc(reset = TRUE, full = TRUE))
   }
 }
