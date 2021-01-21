e2c39f941832a269732a11cdfa9f191fb5dfcebc
Kiernan Nicholls <kiernann@protonmail.com>
2021-01-21 12:32:00 -0500

    Add scale_x_wrap()

--- R/scale-truncate.R
+++ R/scale-truncate.R
@@ -1,13 +1,30 @@
-#' Truncate x-axis labels
+#' Truncate and wrap x-axis labels
 #'
 #' Truncate the labels of a plot's discrete x-axis labels so that the text does
 #' not overflow and collide with other bars.
 #'
-#' @param n The maximum width of string passed to [stringr::str_trunc()].
+#' @param n The maximum width of string. Passed to [stringr::str_trunc()].
+#' @param width Positive integer giving target line width in characters. A width
+#'   less than or equal to 1 will put each word on its own line. Passed to
+#'   [stringr::str_wrap()].
 #' @param ... Additional arguments passed to [ggplot2::scale_x_discrete()].
 #' @importFrom ggplot2 scale_x_discrete
-#' @importFrom stringr str_trunc
+#' @importFrom stringr str_trunc str_wrap
 #' @export
 scale_x_truncate <- function(n = 20, ...) {
-   ggplot2::scale_x_discrete(..., label = function(x) stringr::str_trunc(x, n))
+   ggplot2::scale_x_discrete(
+     label = function(x) {
+       stringr::str_trunc(x, n)
+     }, ...
+   )
+}
+
+#' @rdname scale_x_truncate
+#' @export
+scale_x_wrap <- function(width = 20, ...) {
+  ggplot2::scale_x_discrete(
+    label = function(x) {
+      stringr::str_wrap(x, width)
+    }, ...
+  )
 }
