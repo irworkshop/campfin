53d2732fcd7288a5e072ba79869e73f09dcfc4f0
Kiernan Nicholls <kiernann@protonmail.com>
2022-07-15 12:49:30 -0400

    Remove example from `url_file_size()`

--- R/url-file-size.R
+++ R/url-file-size.R
@@ -7,8 +7,6 @@
 #' @return The size of a file to be downloaded.
 #' @importFrom httr HEAD headers
 #' @importFrom fs as_fs_bytes
-#' @examples
-#' url_file_size("https://cran.r-project.org/bin/macosx/old/R-2.0.1.dmg")
 #' @export
 url_file_size <- function(url) {
   file_head <- httr::HEAD(url = url)
